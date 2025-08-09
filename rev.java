b14590946a8505264cb8445b39a57d728ba8f64b
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-08-09 16:04:32 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,15 @@
+class A7Q6 {
+    public static void main(String[] args) {
+        int[] ar = {1, 2, 5, 7, 3, 6, 3, 6, 68};
+        
+        System.out.println("Array in reverse order:");
+        /*for (int i = ar.length - 1; i >= 0; i--) {
+            System.out.print(ar[i]+" ");
+        }*/
+        for (int i=0,j=9-1;i<j;i++,j--){
+            
+        }
+
+    }
+}/*i=0 and j=n-1. With each iteration i is 
+increased and j is decreased for i<j.  */
