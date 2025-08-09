6ac5c58b72dcfc8cad9c4f4c8b4a71104b6d9bc6
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-08-09 16:07:10 +0530

    Create print.java

---
+++ print.java
@@ -0,0 +1,18 @@
+class Arraytest1{
+    public static void main (String[] args){
+        int[] ar={1,3,4,6};
+        
+        int temp=0;
+        temp=ar[0];
+        for (int i=0;i<ar.length-1;i++){
+            ar[i]=ar[i+1];
+        }
+        ar[ar.length-1]=temp;
+        for(int i=0;i<ar.length;i++){
+           System.out.print(ar[i]+" ");
+        }
+        
+    
+        
+    }
+}
