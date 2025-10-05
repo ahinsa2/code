a20c511e544c422246f3dc994e9e2257e0baa1b0
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-10-05 20:55:38 +0530

    Create circumf.java

---
+++ circumf.java
@@ -0,0 +1,10 @@
+import java.math.*;
+public class Q4 {
+    public static void main(String[] args){
+       circumference(5);
+    }
+    public static void circumference(int r){
+        System.out.println(Math.PI*r*2);
+        return ;
+    }
+}
