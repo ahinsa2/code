c8f9b8e597ef44875cd61a65cbbe64ca09e0cee6
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-10-05 20:56:27 +0530

    Create 18.java

---
+++ 18.java
@@ -0,0 +1,12 @@
+public class Q5 {
+    public static void main(String [] args){
+        vote(20);
+
+    }
+    public static void vote(int age){
+        if(age>=18) 
+        System.out.println("eligible");
+        else
+        System.out.println(" not eligible");
+    }
+}
