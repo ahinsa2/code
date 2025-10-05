24dd6f9148ef53e1d1d374bc3450b5bcbfcede25
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-10-05 20:54:50 +0530

    Create max.java

---
+++ max.java
@@ -0,0 +1,10 @@
+public class Q3 {
+    public static void main(String[] args){
+        System.out.println(greater(2,3));
+        
+    }
+    public static int greater(int a ,int b){
+        if(a>b) return a;
+        else return b;
+    }
+}
