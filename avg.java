d953c2124fdb27d257628e2109bec17b56f64ff5
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-10-05 20:52:37 +0530

    Create avg.java

---
+++ avg.java
@@ -0,0 +1,9 @@
+class Q1{
+    public static void main(String[] args){
+        System.out.print(avg(1,2,3));
+
+    }
+    public static int avg(int a,int b, int c){
+        return (a+b+c)/3;
+    }
+}
